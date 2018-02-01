ac21f7c05fc5ecfc881d3d37ca45eb487c3d7eb9
Himadri Jyoti <himadrisj@gmail.com>
2018-02-01 16:16:45 +0530

    Integrate analytics SDK

---
+++ FlickrSearch/Analytics/Analytics.framework/Headers/Analytics.h
@@ -0,0 +1,18 @@
+//
+//  Analytics.h
+//  Analytics
+//
+//  Created by Test on 01/02/18.
+//  Copyright © 2018 Analytics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Analytics.
+FOUNDATION_EXPORT double AnalyticsVersionNumber;
+
+//! Project version string for Analytics.
+FOUNDATION_EXPORT const unsigned char AnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Analytics/PublicHeader.h>
+
